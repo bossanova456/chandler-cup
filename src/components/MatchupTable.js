5b6adb7ea5d42dce1819b4c1fc06e583eb548841
Taylor Andrews <88749086+bossanova456@users.noreply.github.com>
2023-03-31 23:57:51 -0400

    Added backend connections

--- src/components/MatchupTable.js
+++ src/components/MatchupTable.js
@@ -1,21 +1,24 @@
 import MatchupTableRow from "./MatchupTableRow";
 
-import { pickData } from "../PickData";
-
 import { useEffect, useState } from "react";
 
-const getMatchupPick = (matchupId) => {
-	return pickData[0].weeklyPicks.filter(week => week.weekNum === 0)[0]
-		.matchUps.filter(matchup => matchup.matchUpId === matchupId)[0]
-		.pick;
-}
-
 const MatchupTable = () => {
+	const [ teams, setTeams ] = useState({});
 	const [ matchups, setMatchups ] = useState([]);
 	const [ picks, setPicks ] = useState({});
 	const [ year, setYear ] = useState('');
 	const [ week, setWeek ] = useState('01');
 	const [ currentSeason, setCurrentSeason ] = useState({});
+	const [ currentUser, setCurrentUser ] = useState('sharon');
+
+	useEffect(() => {
+		fetch('http://192.168.1.51:3001/teams')
+			.then(response => response.json())
+			.then(data => {
+				console.log("Teams: " + JSON.stringify(data));
+				setTeams(data);
+			});
+	}, []);
 
 	useEffect(() => {
 		fetch('http://192.168.1.51:3001/currentSeason')
@@ -29,26 +32,24 @@ const MatchupTable = () => {
 
 	// Should use current year data from call
 	useEffect(() => {
-		fetch('http://192.168.1.51:3001/matchups/year/' + year + '/week/' + week)
-			.then(response => response.json())
-			.then(data => {
-				setMatchups(data);
-			}, () => {
-				console.error("Error: could not get matchup data (year: " + year + ", week: " + week + ")");
-			});
+		if (year && week) {
+			fetch('http://192.168.1.51:3001/matchups/year/' + year + '/week/' + week)
+				.then(response => response.json())
+				.then(data => {
+					setMatchups(data);
+				}, () => {
+					console.error("Error: could not get matchup data (year: " + year + ", week: " + week + ")");
+				});
 
-		fetch('http://192.168.1.51:3001/picks/year/' + year + '/week/' + week)
-			.then(response => response.json())
-			.then(data => {
-				setPicks(data);
-			}, () => {
-				console.error("Error: pick data not found (year: " + year + ", week: " + week + ")");
-			});
-	}, [ year, week ]);
-
-	// Should use current year data from call
-	useEffect(() => {
-		
+			fetch('http://192.168.1.51:3001/picks/year/' + year + '/week/' + week)
+				.then(response => response.json())
+				.then(data => {
+					console.log("Picks: " + JSON.stringify(data));
+					setPicks(data);
+				}, () => {
+					console.error("Error: pick data not found (year: " + year + ", week: " + week + ")");
+				});
+		}
 	}, [ year, week ]);
 	
 	return (
@@ -60,6 +61,7 @@ const MatchupTable = () => {
 						<th>Underdog</th>
 						<th>Line</th>
 						<th>Outcome</th>
+						<th>Pick</th>
 					</tr>
 				</thead>
 				<tbody>
@@ -68,7 +70,9 @@ const MatchupTable = () => {
 							return <MatchupTableRow 
 										key={"" + i}
 										matchup={matchup}
-										userPick={getMatchupPick(matchup.favoredTeamId + matchup.underdogTeamId)}
+										userPick={picks[currentUser] ? picks[currentUser][matchup.favoredTeamId + matchup.underdogTeamId] : {}}
+										favoredTeam={teams[matchup.favoredTeamId]}
+										underdogTeam={teams[matchup.underdogTeamId]}
 									/>
 						})
 					}
